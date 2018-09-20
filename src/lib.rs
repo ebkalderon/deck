37747c5e6bd7492c8114e8d40aa975066e2ff6b6
Eyal Kalderon <ebkalderon@gmail.com>
2018-09-21 03:55:41 +0800

    Start completely fresh, add some PoCs

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,11 @@
+//! Deck build daemon.
+
 #[macro_use]
 extern crate failure;
 extern crate license_exprs;
-extern crate ron;
 extern crate semver;
 #[macro_use]
 extern crate serde;
+extern crate toml;
 extern crate url;
 extern crate url_serde;
-
-use std::iter::FromIterator;
-use std::marker::PhantomData;
-use std::vec::IntoIter;
-
-use self::package_id::PackageId;
-use self::package_id_spec::PackageIdSpec;
-
-pub mod license;
-pub mod manifest;
-pub mod package_id;
-pub mod package_id_spec;
-
-#[derive(Debug)]
-pub struct Installed<'p> {
-    packages: IntoIter<PackageId<'p>>,
-    marker: PhantomData<&'p ()>
-}
-
-impl<'p> Iterator for Installed<'p> {
-    type Item = PackageId<'p>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.packages.next()
-   }
-}
-
-#[derive(Debug, Fail)]
-pub enum InstallError {
-    #[fail(display = "package ID error")]
-    PackageId,
-    #[fail(display = "build of package {} failed: {}", package_id, msg)]
-    BuildFailed {
-        package_id: String,
-        msg: String,
-    }
-}
-
-pub fn install<'p, P>(_pkgs: P) -> Result<Installed<'p>, InstallError>
-where
-    P: FromIterator<PackageIdSpec<'p>>
-{
-    Ok(Installed {
-        packages: Vec::new().into_iter(),
-        marker: PhantomData
-    })
-}
