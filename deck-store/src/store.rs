b7f45ef2bef0246f640796c315926d88fdf561d9
Eyal Kalderon <ebkalderon@gmail.com>
2019-02-05 21:38:41 +0800

    Rewrite Store trait to use std::future::Future

--- deck-store/src/store.rs
+++ deck-store/src/store.rs
@@ -2,8 +2,8 @@ pub use self::builder::BuildStream;
 pub use self::closure::Closure;
 
 use std::fmt::Debug;
-
-use futures::Future;
+use std::future::Future;
+use std::pin::Pin;
 
 use crate::binary_cache::BinaryCache;
 use crate::package::Manifest;
@@ -17,32 +17,34 @@ mod closure;
 mod context;
 mod fs;
 
-pub type PlatformFuture = Box<dyn Future<Item = Vec<Platform>, Error = ()>>;
+// NOTE: All this noise has been to work fine with a simple `async fn`, with no need for associated
+// types, this type alias, or `Pin<Box<_>>`. Replace _immediately_ once `async fn` in traits is
+// stabilized in Rust.
 
-pub type AddedFuture = Box<dyn Future<Item = (), Error = ()>>;
+pub type StoreFuture<'a, T> = Pin<Box<dyn Future<Output = Result<T, ()>> + Send + 'a>>;
 
-pub type VerifyFuture = Box<dyn Future<Item = (), Error = ()>>;
-
+/// Sets whether the hashes of the store contents should be recomputed and verified.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum CheckContents {
+    /// Each item in the store should have its hash recomputed and verified.
     Enabled,
+    /// Only check whether the paths are registered, do not validate the hashes.
     Disabled,
 }
 
+/// Sets whether store inconsistencies should be repaired.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Repair {
+    /// Missing paths should be registered and inconsistent hashes should be recomputed.
     Enabled,
+    /// Nothing should be repaired, report errors without modifying the store.
     Disabled,
 }
 
 pub trait Store: Debug {
-    fn supported_platforms(&self) -> PlatformFuture;
-
-    fn build_package(&mut self, manifest: &Manifest) -> BuildStream;
-
-    fn add_binary_cache<B: BinaryCache>(&mut self, cache: B) -> AddedFuture;
-
-    fn add_remote_store<S: Store>(&mut self, store: S) -> AddedFuture;
-
-    fn verify(&mut self, check: CheckContents, repair: Repair) -> VerifyFuture;
+    fn supported_platforms<'a>(&'a self) -> StoreFuture<'a, Vec<Platform>>;
+    fn build_package(&mut self, manifest: Manifest) -> BuildStream;
+    fn add_binary_cache<'a, B: BinaryCache>(&'a mut self, cache: B) -> StoreFuture<'a, ()>;
+    fn add_remote_store<'a, S: Store>(&'a mut self, store: S) -> StoreFuture<'a, ()>;
+    fn verify<'a>(&'a mut self, check: CheckContents, repair: Repair) -> StoreFuture<'a, ()>;
 }
